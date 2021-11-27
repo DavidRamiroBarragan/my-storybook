0d0fd795f6f0fd58628c6da04981d33bfdde7fe7
David Ramiro Barragan <dramirobarragan@gmail.com>
2021-11-27 10:05:36 +0100

    my first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,42 @@
+import './MyLabel.css';
+
+export interface MyLabelProps {
+  /**
+   * Texto que se mostrará en la etiqueta
+   */
+  label: string;
+  /**
+   * Tamaño de la etiqueta
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Color del botón
+   */
+  color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * Capitalizar todas las letras
+   */
+  allCaps?: boolean;
+
+  /**
+   * Color de la fuente
+   */
+  fontColor?: string;
+}
+export const MyLabel = ({
+  allCaps,
+  color = 'primary',
+  label = 'no label',
+  size = 'normal',
+  fontColor,
+}: MyLabelProps) => {
+  const texto = !!allCaps ? label.toLocaleUpperCase() : label;
+  return (
+    <span
+      className={['label', size, `text-${color}`].join(' ')}
+      style={{ color: fontColor }}
+    >
+      {texto}
+    </span>
+  );
+};
